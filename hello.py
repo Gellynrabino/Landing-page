5c18d5d8be1b02cf76939a50f4abdf7fc09d5499
Gellynrabino <gellynrabino5@gmail.com>
2025-02-06 16:28:12 +0800

    MyLandingpage

---
+++ hello.py
@@ -0,0 +1,9 @@
+students = {"Abuton","Batumbakal","Lore"}
+print(students)
+students.add("Calma")
+print(students.add)
+students.update(["Patoy","Lumasag"])
+print(students.update)
+len(students)
+print(len(students))
+print(students)
